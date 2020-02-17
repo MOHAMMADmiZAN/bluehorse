f5f6240afd88fdacce652205cded7539ce40d1b3
MOHAMMAD MIZAN <mohammad25524143@gmail.com>
2020-02-18 00:30:35 +0600

    self

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function ()
+{ $(".sw").click(function () {
+        $("#menu").fadeOut();
+        $(".sw").fadeOut();
+});
+    $(".hd").click(function () {
+        $("#menu").fadeIn();
+        $(".sw").fadeIn();
+    });
+
+});
+$(document).ready(function () {
+$(".ser").click(function () {
+    $(".menu-itm").fadeToggle();
+
+});
+});
+$(document).ready(function () {
+    $(".srv").click(function () {
+        $("#opacity").fadeIn();
+    });
+    $(".srv").click(function () {
+        $("#plug").hide();
+    });
+    $(".srv").click(function () {
+        $("#menu").hide();
+    });
+   $(".error").click(function () {
+       $("#opacity").fadeOut();
+
+   });
+    $(".error").click(function () {
+        $("#plug").show();
+
+    });
+    $(".error").click(function () {
+        $("#menu").show();
+
+    });
+});
+
